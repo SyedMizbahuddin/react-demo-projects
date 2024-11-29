05c8e750d9e678158be442ed5d48498c3b2f3e09
Syed Mizbahuddin <smizbahu@qti.qualcomm.com>
2024-11-29 18:16:23 +0530

    ADDED star rating

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import Accordion from './component/accordion';
-import RandomColor from './component/random-color';
+import Accordion from "./component/accordion";
+import RandomColor from "./component/random-color";
+import StarRating from "./component/star-rating";
 
 function App() {
-  return (<>
-  <RandomColor />
-  <Accordion />
-  </>
-    
+  return (
+    <>
+      <StarRating noOfStars={10} />
+      <RandomColor />
+      <Accordion />
+    </>
   );
 }
 
--- src/component/accordion/index.jsx
+++ src/component/accordion/index.jsx
@@ -1,52 +1,60 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
 import data from "./data";
-import './styles.css';
-
+import "./styles.css";
 
 export default function Accordion() {
   const [selected, setSelected] = useState([]);
   const [multiSelect, setMultiSelect] = useState(false);
 
   const handleQuestionClick = (id) => {
-    if(multiSelect){
-        handleMultiSelect(id);
-    }else{
-        handleSingleSelect(id);
+    if (multiSelect) {
+      handleMultiSelect(id);
+    } else {
+      handleSingleSelect(id);
     }
-  }
+  };
 
-  const handleSingleSelect = (id) =>{
-    if(selected.includes(id)){
-        setSelected(null);
-    }else{
-        setSelected([id]);
+  const handleSingleSelect = (id) => {
+    if (selected.includes(id)) {
+      setSelected(null);
+    } else {
+      setSelected([id]);
     }
-  }
-  const handleMultiSelect = (id) =>{
-    if(selected.includes(id)){
-        setSelected(selected.filter((idd) => idd !== id));
-    }else{
-        setSelected([...selected,id]);
+  };
+  const handleMultiSelect = (id) => {
+    if (selected.includes(id)) {
+      setSelected(selected.filter((idd) => idd !== id));
+    } else {
+      setSelected([...selected, id]);
     }
-  }
+  };
 
   return (
     <div className="wrapper">
-        <button onClick={() => {setMultiSelect(!multiSelect); setSelected([]) } }> Enable Multi Selection</button>
+      <button
+        onClick={() => {
+          setMultiSelect(!multiSelect);
+          setSelected([]);
+        }}
+      >
+        {" "}
+        Enable Multi Selection
+      </button>
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((item) => (
-            <div className="item">
-              <div onClick={() => handleQuestionClick(item.id)}className="title">
+            <div className="item" key={item.id}>
+              <div
+                onClick={() => handleQuestionClick(item.id)}
+                className="title"
+              >
                 {item.question}
                 <span>+</span>
-                </div>
-                {
-                    selected.includes(item.id) ?
-                    <div className="content">{item.answer}</div>
-                    : null
-                }
+              </div>
+              {selected.includes(item.id) ? (
+                <div className="content">{item.answer}</div>
+              ) : null}
             </div>
           ))
         ) : (
